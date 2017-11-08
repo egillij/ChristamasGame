b547cd99b3a0f3011aa5672553f2584738bb3054
qotsafan1 <sigurbjornjonss@gmail.com>
2017-11-08 15:27:57 +0100

    Menu/ChangeScenes

---
+++ ChristmasPlatformer/Assets/Scripts/ChangeScenes.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeScenes : MonoBehaviour
+{
+
+    public void LoadScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
