6c3747f9b9625032226723c893b2073816be1639
qotsafan1 <sigurbjornjonss@gmail.com>
2017-11-12 22:24:18 +0100

    Small updates. Game manager setup

---
+++ ChristmasPlatformer/Assets/Scripts/GameManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    public static GameManager instance;
+
+    public int score;
+
+    void Awake()
+    {
+        MakeSingleton();
+    }
+
+    private void MakeSingleton()
+    {
+        if (instance != null)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+    
+}
