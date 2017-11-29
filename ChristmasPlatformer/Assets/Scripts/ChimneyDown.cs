34fe2ee764abda4a5c4e194fa55daa770708dc7b
qotsafan1 <sigurbjornjonss@gmail.com>
2017-11-29 14:17:48 +0100

    End to Level1. Chimney scene changer.

---
+++ ChristmasPlatformer/Assets/Scripts/ChimneyDown.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChimneyDown : MonoBehaviour
+{
+
+    [SerializeField]
+    private string sceneName;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            //LibPD.SendFloat("banger", 1f);
+            //LibPD.SendBang("banger");
+
+            SceneManager.LoadScene(sceneName);
+        }
+    }
+}
