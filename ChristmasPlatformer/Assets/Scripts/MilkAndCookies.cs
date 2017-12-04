761ea660217c614f167abecdaa84506288da3d04
qotsafan1 <sigurbjornjonss@gmail.com>
2017-12-04 21:03:45 +0100

    MilkAndCookies Prefab created. Gives player extra health.

---
+++ ChristmasPlatformer/Assets/Scripts/MilkAndCookies.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MilkAndCookies : MonoBehaviour {
+
+    int direction = 1;
+    DateTime turnAroundTime;
+
+    void Start()
+    {
+        turnAroundTime = DateTime.Now;
+    }
+
+	void FixedUpdate ()
+    {
+        MoveUpAndDown();
+	}
+
+    void MoveUpAndDown()
+    {        
+        if (DateTime.Now > turnAroundTime)
+        {
+            direction *= -1;
+            turnAroundTime = turnAroundTime.AddSeconds(1);
+        }
+        
+        transform.position = new Vector2(transform.position.x, transform.position.y + (0.02f * direction));
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            GameManager.instance.health++;
+            Destroy(gameObject);
+        }        
+    }
+}
