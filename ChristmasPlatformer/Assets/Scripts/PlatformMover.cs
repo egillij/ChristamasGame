40843ee77dbdc14d2d4e101078d611b54e89a2d2
qotsafan1 <sigurbjornjonss@gmail.com>
2017-12-14 16:08:41 +0100

    PlatformMover fixed.

--- ChristmasPlatformer/Assets/Scripts/PlatformMover.cs
+++ ChristmasPlatformer/Assets/Scripts/PlatformMover.cs
@@ -25,29 +25,19 @@ public class PlatformMover : MonoBehaviour {
 	// Update is called once per frame
 	void LateUpdate () {
         Vector2 platform2DPosition = new Vector2(gameObject.transform.position.x, gameObject.transform.position.y);
+        
+        if ((platform2DPosition - originPosition).magnitude >= range
+            && ((platform2DPosition.x - originPosition.x > 0f && movingDirection.x > 0f)
+            || (platform2DPosition.x - originPosition.x < 0f && movingDirection.x < 0f)
+            || (platform2DPosition.y - originPosition.y > 0f && movingDirection.y > 0f)
+            || (platform2DPosition.y - originPosition.y < 0f && movingDirection.y < 0f))
+            ) {
 
-        if ((platform2DPosition - originPosition).magnitude >= range)
-        {
             movingDirection = -1.0f * movingDirection;
             rb.velocity = Vector2.zero;
-            //if (playerOnTop)
-            //{
-            //    pBody.velocity = Vector2.zero;
-            //}
         }
-        //rb.velocity = movingDirection * speed;
-        rb.transform.Translate(movingDirection * speed * Time.deltaTime);
-        //rb.MovePosition(platform2DPosition + movingDirection * speed * Time.fixedDeltaTime);
-        //rb.AddForce(movingDirection * speed);
-
-        //if (playerOnTop)
-        //{
-        //    if (!Player.Instance.Jump)
-        //    {
-        //        //pBody.MovePosition(pBody.position + movingDirection * speed * Time.fixedDeltaTime);
-        //        pBody.transform.Translate(movingDirection * speed * Time.fixedDeltaTime);
-        //    }
-        //}
+        
+        rb.transform.Translate(movingDirection * speed * Time.deltaTime);        
     }
 
     public void OnCollisionEnter2D(Collision2D collision)
@@ -56,9 +46,7 @@ public class PlatformMover : MonoBehaviour {
         if (collision.gameObject.tag == "Player" && !playerOnTop)
         {
             playerOnTop = true;
-            Player.Instance.MovingPlatform = this.gameObject;
-            //pBody = collision.gameObject.GetComponent<Rigidbody2D>();
-            //pBody.velocity = Vector2.zero;
+            Player.Instance.MovingPlatform = this.gameObject;            
             collision.gameObject.transform.parent = this.transform;
         }
     }
@@ -68,9 +56,7 @@ public class PlatformMover : MonoBehaviour {
         if (collision.gameObject.tag == "Player" && !playerOnTop)
         {
             playerOnTop = true;
-            Player.Instance.MovingPlatform = this.gameObject;
-            //pBody = collision.gameObject.GetComponent<Rigidbody2D>();
-            //pBody.velocity = Vector2.zero;
+            Player.Instance.MovingPlatform = this.gameObject;            
 
             collision.gameObject.transform.parent = this.transform;
         }
@@ -80,8 +66,7 @@ public class PlatformMover : MonoBehaviour {
     {
         if (collision.gameObject.tag == "Player")
         {
-            playerOnTop = false;
-            //pBody = null;
+            playerOnTop = false;            
             Player.Instance.MovingPlatform = null;
 
             collision.gameObject.transform.parent = null;
